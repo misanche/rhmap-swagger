3c47ffd8df7421a7526d080f26b1fdaa8d7bfc5d
BP1\hemosq <Mikel.Herrero@bp.com>
2017-10-31 11:49:01 +0000

    updated package

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ module.exports = function(app, options) {
   var swaggerSpec = swaggerJSDoc(options);
 
   app.use('/api-docs', swaggerUI.serve);
-  app.use('api-docs', swaggerUI.setup(swaggerSpec));
+  app.use('/api-docs', swaggerUI.setup(swaggerSpec));
 
-  app.get('api-docs.json', function(req, res) {
+  app.get('/api-docs.json', function(req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send(swaggerSpec);
   });
